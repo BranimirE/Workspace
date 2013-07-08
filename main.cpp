0faaa0bd3114ff2a0dc3ab91aa9fc027b48ce7b9
BranimirE <brani.espinoza@gmail.com>
2013-07-08 11:10:31 -0400

    Otro cambio

--- main.cpp
+++ main.cpp
@@ -4,11 +4,24 @@
 #include <map>
 #include <set>
 #include <queue>
+#include <climits>
+#include <cmath>
+#include <sstream>
+
+#define PI acos(-1)
+#define INF 1<<30
+#define x first
+#define y second
 
 using namespace std;
 
+string toStr(int n){stringstream ss;ss << n; return ss.str();}
+int toInt(string c){stringstream ss(c);int t;ss>>t; return t;}
+
+
 int main(){
 	freopen("entrada.in", "r", stdin);
-
+	
+	
 	return 0;
 }
