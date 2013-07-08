6796c67b8296dfff41bcb5e28cbb6b75a2f84ef3
BranimirE <brani.espinoza@gmail.com>
2013-07-08 10:27:56 -0400

    primer cambio

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
 
 using namespace std;
 
 int main(){
-	freopen("entrada.in");;
+	freopen("entrada.in", "r", stdin);
+
 	return 0;
 }
