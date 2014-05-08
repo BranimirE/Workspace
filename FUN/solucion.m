534a1ee3b12995717a65f00f3d279e3960c0f62d
BranimirE <brani.espinoza@gmail.com>
2014-05-08 10:51:37 -0400

    Ultima modificacion para el metodo de newton para resolver sistemas de ecuaciones no lineales :D

--- FUN/solucion.m
+++ FUN/solucion.m
@@ -3,15 +3,12 @@ function solucion()
     
     % LECTURA DE ECUACIONES
     
-    n = input('Ingrese el numero de ecuaciones del sistema: ');
+    n = input('Ingrese el numero de ecuaciones: ');
     F = sym([n]);
     disp('Ahora ingrese las ecuaciones en la forma F(X) = 0 (Ingrese F(X))');
     for i = 1:n
-        F(i) = sym(input('Ecuación: ', 's'));
+        F(i)=sym(input('Ecuación: ', 's'));
     end
-    
-    %F = [sym('x*x + y*y - 10'), sym('x*x - y*y - 1')];
-    %n = length(F);
     
     % RECOLECCION DE VARIABLES
     variables = [];
@@ -34,7 +31,8 @@ function solucion()
     for i = 1:n
         X(i) = input([char(variables(i)), ' : ']);
     end
-    
+    %    LECTURA DE LA TOLERANCIA
+    tolerancia = input('Ingrese la tolerancia: ');
     %X = [1 1];
     
     X = X';% Ya que X es un vector fila lo trasponemos para poder
@@ -42,7 +40,7 @@ function solucion()
     
     % ITERACIONES
     
-    iteraciones = input('Ingrese el numero de iteraciones que desea: ');
+    iteraciones = input('Ingrese el numero maximo de iteraciones que desea realizar: ');
     %iteraciones = 5;
     
     clc
@@ -61,17 +59,21 @@ function solucion()
     
     fprintf('%5s','n');
     for i = 1:n
-        fprintf('%10s', [char(variables(i)),'[n]']);
+        fprintf('%13s', [char(variables(i)),'[n]']);
+    end
+    for i = 1:n
+        fprintf('%15s', ['Error(',char(variables(i)),')']);
     end
     fprintf('\n');
+    
     fprintf('%5d', 0);
     for i = 1:n
-        fprintf('%10.5f ', X(i));
+        fprintf('%13.8f ', X(i));
     end
     fprintf('\n');
     
     %    CALCULO DE ITERACIONES
-    
+    error = [];
     for iteracion = 1:iteraciones
         % Evaluamos en el jacobiano
         for i = 1:n
@@ -86,16 +88,32 @@ function solucion()
             Fx(i) = subs(F(i), variables, X);
         end
         Fx = Fx'; % Ya que Fx es un vector fila lo trasponemos para poder
-                  % obtener un vector columna, para poder operar
-                  
+                  % obtener un vector columna para poder operar
+        
+        previo = X; % Guardamos el valor previo de X para calcular el error
+        X = (X - inv(evaljac) * Fx);% <-------- APLICAMOS NEWTON
+        
+        
         % Imprimimos el resultado de la iteración
-        
-        X = (X - inv(evaljac) * Fx);% <-------- NEWTON!!!
-        
         fprintf('%5d', iteracion);
         for i = 1:n
-            fprintf('%10.5f ', X(i));
+            fprintf('%13.8f ', X(i));
+        end
+        maximoerror = 0;
+        for i = 1:n
+            error(i) = abs(X(i) - previo(i));
+            if error(i) > maximoerror
+                maximoerror = error(i);
+            end
+            fprintf('%13.8f ', error(i));
         end
         fprintf('\n');
+        if maximoerror <= tolerancia
+            disp('Proceso terminado satisfactoriamente :D');
+            break;
+        end
+    end
+    if maximoerror > tolerancia
+        disp('Es necesario mas iteraciones :(');
     end
 end
